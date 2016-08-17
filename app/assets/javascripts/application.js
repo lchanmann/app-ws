0cf81e091499c1619bf8e3ccbc9ca408e16999a7
Mann <chanmannlim@gmail.com>
2016-08-17 17:23:30 -0400

    sample code for window.fetch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,34 @@ $(function() {
   // update current queries every 3 seconds
   setInterval(window.App.updateCurrentQueries, 3000);
 });
+
+// window.App = {
+//   currentQueries: function() {
+//     if (document.querySelector('.queries')) {
+//       fetch(window.location.href, {
+//         headers: {
+//           'X-Requested-With': 'XMLHttpRequest',
+//           'Accept': 'application/javascript',
+//           'Content-Type': 'application/javascript'
+//         }
+//       }).then(function(response) {
+//         if (response.ok)
+//           return response.text();
+//       }).then(function(text) {
+//         eval(text);
+//       });
+//     }
+//   }
+// }
+// 
+// updateCurrentQueries = function() {
+//   setInterval(window.App.currentQueries, 3000);
+// }
+// 
+// // onload (copy from -> https://github.com/spree/spree/pull/6670)
+// if (window.addEventListener)
+//   window.addEventListener("load", updateCurrentQueries, "false");
+// else if (window.attachEvent)
+//   window.attachEvent("onload", updateCurrentQueries);
+// else  window.onload = updateCurrentQueries;
+// 
